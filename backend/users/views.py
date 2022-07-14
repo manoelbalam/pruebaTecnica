cf469593ab35ac0f1bf9038f0709221d12228dd0
Thare Valverde <thare.valverde@mxtech.group>
2022-07-14 15:23:31 -0500

    login endpoint basic

--- backend/users/views.py
+++ backend/users/views.py
@@ -24,9 +24,11 @@ def user_signup(request):
 
 @api_view(['POST'])
 def user_login(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
-        
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        user = User.objects.get(UserAccess=request.data.get("UserAccess"),PassAccess=request.data.get("PassAccess"))
+    except User.DoesNotExist:
+        return Response({"estado":"false","descripcionRespuesta":"Usuario o contrasena incorrecto"},status=status.HTTP_404_NOT_FOUND)
+
+    serializer = UserSerializer(user)
+
+    return Response({"estado":"true","descripcionRespuesta":"","token":serializer.data.get("TokenAccess")})
