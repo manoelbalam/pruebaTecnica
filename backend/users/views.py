2364570b657edb6d15ea7030c9e976ca64063570
Thare Valverde <thare.valverde@mxtech.group>
2022-07-14 16:49:39 -0500

    adding renew date and token on expired

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-import pdb
+from django.forms.models import model_to_dict
+from datetime import date, datetime, timedelta
+import uuid
 
 from .models import User
 from .serializers import *
@@ -11,10 +13,13 @@ def user_signup(request):
     if request.method == 'GET':
         data = User.objects.all()
         serializer = UserSerializer(data, context={'request': request}, many=True)
-
         return Response(serializer.data)
 
     elif request.method == 'POST':
+        if "ExpirationAccess" not in request.data:
+            request.data["ExpirationAccess"] = date.today() + timedelta(weeks=2)
+
+        request.data["TokenAccess"] = str(uuid.uuid4())
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -29,6 +34,12 @@ def user_login(request):
     except User.DoesNotExist:
         return Response({"estado":"false","descripcionRespuesta":"Usuario o contrasena incorrecto"},status=status.HTTP_404_NOT_FOUND)
 
-    serializer = UserSerializer(user)
+    if datetime.now() > datetime.combine(user.ExpirationAccess, datetime.min.time()):
+        user.ExpirationAccess = date.today() + timedelta(weeks=2)
+        user.TokenAccess = str(uuid.uuid4())
+
+        serializer = UserSerializer(user, data=model_to_dict(user))
+        serializer.is_valid()
+        serializer.save()
 
     return Response({"estado":"true","descripcionRespuesta":"","token":serializer.data.get("TokenAccess")})
